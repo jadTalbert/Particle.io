2e47f96217eb9e8a6d06076055280c677a1ae0e8
Jad Talbert <jad.talbert@gmail.com>
2020-09-16 22:51:39 -0400

    removing JSON payload and replacing with deviceOwner variable to hold the owner of the wearable

--- Boron/pushButtonToPublish.ino
+++ Boron/pushButtonToPublish.ino
@@ -3,6 +3,8 @@ int led1 = D0;
 //write to D1 when the button is pressed
 int buttonClick = D1;
 
+String deviceOwner = "Miggie Hedrick"; 
+
 //set up
 void setup() {
     pinMode(led1, OUTPUT);
@@ -18,11 +20,10 @@ void loop() {
   //while loop to continously determine if the button has been pressed. If voltage is on the pin, then publish to the particle cloud
   while(digitalRead(buttonClick) == HIGH) {
       
-     //JSONValue outerObj = JSONValue::parseCopy( String("[\"abc\",\"def\",\"xxx\"]"));
-     String jsonDataString = String( "{ \"name\":" + String("YourStringHere") + ",\"event\":" + String("YourEventHere") +"}");
-
+   
+    
 	//publish to the Particle.io cloud
-    Particle.publish("Fall Detected",jsonDataString, 3600, PRIVATE); 
+    Particle.publish("Fall Detected",deviceOwner, 3600, PRIVATE); 
     delay(2000); 
   }//end while loop
   
